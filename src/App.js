1319a70f7c217b73a2735603b5390b4322190de8
AlexIrungu <aleaxmuiruri@gmail.com>
2023-07-27 15:42:38 +0300

    started on tourism project. added routes and created components

---
+++ src/App.js
@@ -0,0 +1,37 @@
+//import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Faq from './components/FAQ';
+import Service from './components/Service';
+import Gallery from './components/Gallery';
+import LandingPage from './components/LandingPage'
+
+
+function App() {
+  return (
+    <div className='App' >
+      <Navbar />
+    <Switch>
+      <Route path="/" exact component={LandingPage} />
+      {/* <Route component={Navbar} /> */}
+      <Route path="/home" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/service" exact component={Service} />
+      <Route path="/gallery" exact component={Gallery} />
+      <Route path="/faq" exact component={Faq} />
+      <Route path="/contact" exact component={Contact} />
+    </Switch>
+    </div>
+   
+      
+     
+   
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage(){
+    return(
+        <div className="landing-page">
+            <h1>Welcome to our tourism website</h1>
+
+        </div>
+    )
+}
+
+export default LandingPage
