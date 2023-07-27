cc7d806b70ea876cf41fd246c8006cf50ad7060b
AlexIrungu <aleaxmuiruri@gmail.com>
2023-07-27 18:17:41 +0300

    added a follow us section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import './LandingPage.css'
+
 function LandingPage(){
     return(
        
@@ -25,8 +33,43 @@ function LandingPage(){
           <div className="image-gallery">
             {/* Add your images here */}
             <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/Masai_Mara_at_Sunset.jpg" alt="Image 1" />
-            <img src="/images/image2.jpg" alt="Image 2" />
-            <img src="/images/image3.jpg" alt="Image 3" />
+            {/* <img src="/images/image2.jpg" alt="Image 2" />
+            <img src="/images/image3.jpg" alt="Image 3" /> */}
+          </div>
+        </div>
+      </section>
+      <section className="follow-us-section">
+        <div className="container">
+          <h2>Follow Us</h2>
+          <div className="social-icons">
+            <a
+              href="https://twitter.com/your_twitter_handle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a
+              href="https://www.facebook.com/your_facebook_page"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              href="https://www.instagram.com/your_instagram_handle"
+              target="_alexirungu"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a
+              href="https://www.linkedin.com/your_linkedin_profile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
           </div>
         </div>
       </section>
